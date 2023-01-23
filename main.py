78192b41a333640fb39afb9b139346e08db679b7
Rafael <bboyrafinhazika@gmail.com>
2023-01-23 18:41:42 -0300

    sérias mudanças

--- main.py
+++ main.py
@@ -6,6 +6,12 @@ import telegram_foward
 import planilha_api
 import time
 
+URL_NOTEBOOKS_PARSED = {'Notebook': ['https://www.kabum.com.br/computadores/notebooks?page_number=', '&page_size=100&facet_filters=eyJwcmljZSI6eyJtaW4iOjE0NDUuNDksIm1heCI6MTE1MzUuMjF9fQ==&sort=price'],
+                        'Monitor': ['https://www.kabum.com.br/computadores/monitores?page_number=', '&page_size=100&facet_filters=eyJwcmljZSI6eyJtaW4iOjc1LjA1LCJtYXgiOjI3MTguNTR9fQ==&sort=price'],
+                        'Mouse': ['https://www.kabum.com.br/busca/mouse?page_number=', '&page_size=100&facet_filters=eyJwcmljZSI6eyJtaW4iOjY2LjcsIm1heCI6NjQzLjM5fX0=&sort=price'],
+                        'Teclado':  ['https://www.kabum.com.br/perifericos/teclado-gamer?page_number=', '&page_size=100&facet_filters=eyJwcmljZSI6eyJtaW4iOjc1LjYzLCJtYXgiOjY5OC40OX19&sort=price'],
+                        'Headset': ['https://www.kabum.com.br/perifericos/headset-gamer?page_number=', '&page_size=100&facet_filters=eyJwcmljZSI6eyJtaW4iOjcwLjEsIm1heCI6NjY2LjAzfX0=&sort=price'] 
+                    }
 
 def get_response(url):
     ua = UserAgent()
@@ -17,72 +23,51 @@ def get_response(url):
         exit()
     return response
 
-url_by_category = {
-    'notebook': ['https://www.kabum.com.br/computadores/notebooks?page_number=', '&page_size=100&facet_filters=eyJwcmljZSI6eyJtaW4iOjE0NDUuNDksIm1heCI6MTE1MzUuMjF9fQ==&sort=most_searched'],
-    'monitor': ['https://www.kabum.com.br/computadores/monitores?page_number=', '&page_size=100&facet_filters=eyJwcmljZSI6eyJtaW4iOjc1LjA1LCJtYXgiOjI3MTguNTR9fQ==&sort=most_searched'],
-    'mouse': ['https://www.kabum.com.br/busca/mouse?page_number=', '&page_size=100&facet_filters=eyJwcmljZSI6eyJtaW4iOjY2LjcsIm1heCI6NjQzLjM5fX0=&sort=most_searched'],
-    'teclado':  ['https://www.kabum.com.br/perifericos/teclado-gamer?page_number=', '&page_size=100&facet_filters=eyJwcmljZSI6eyJtaW4iOjc1LjYzLCJtYXgiOjY5OC40OX19&sort=most_searched'],
-    'headset': ['https://www.kabum.com.br/perifericos/headset-gamer?page_number=', '&page_size=100&facet_filters=eyJwcmljZSI6eyJtaW4iOjcwLjEsIm1heCI6NjY2LjAzfX0=&sort=most_searched']    
-}
+def send_product_data(nome, id, preco):
+    data = {'ID': id,
+            'Título': nome,
+            'Preço': preco}
+    print(data)
 
 def main():
     while True:
-        planilha, service = planilha_api.main()
-        tags = {}
-        for i in range(len(planilha)):
-            try:
-                tags[planilha[i][0].lower()].append({'modelo': planilha[i][1],
-                                                    'referencia': planilha[i][3],
-                                                    'price': [float(x) for x in planilha[i][4].strip('R$').strip().split(',')[:-1]][0]*1000,
-                                                    'linha': str(i+5)})
-            except:
-                try:
-                    tags[planilha[i][0].lower()] = [{'modelo': planilha[i][1],
-                                                    'referencia': planilha[i][3],
-                                                    'price': [float(x) for x in planilha[i][4].strip('R$').strip().split(',')[:-1]][0]*1000,
-                                                    'linha': str(i+5)}]
-                except:
-                    pass
-
-        for tag in tags.keys():
-            try:
-                url_inicial = url_by_category[tag][0] + '1' + url_by_category[tag][1]
-                response = get_response(url_inicial)
+        try:
+            planilha = planilha_api.main()
+            for i in range(len(planilha)):
+                codigo_do_produto = planilha[i][1]
+                referencia = planilha[i][3]
+                old_price = [float(x) for x in planilha[i][4].strip('R$').strip().split(',')[:-1]][0]*1000
+                categoria = planilha[i][0]
+                url = URL_NOTEBOOKS_PARSED[categoria][0] + '1' + URL_NOTEBOOKS_PARSED[categoria][1]
+                response = get_response(url)
                 site = BeautifulSoup(response.content, 'html.parser')
                 qtd_produtos = site.find('div', id='listingCount').text.strip()
                 index = qtd_produtos.index(' ')
                 qtd_produtos = qtd_produtos[:index]
                 num_paginas = int(int(qtd_produtos)/100) + 1
-                for i in range(1, num_paginas + 1):
-                    url = url_by_category[tag][0] + str(i) + url_by_category[tag][1]
+                price_value = 100000
+                n = 0
+                while n < num_paginas:
+                    n += 1
+                    url = URL_NOTEBOOKS_PARSED[categoria][0] + str(n) + URL_NOTEBOOKS_PARSED[categoria][1]
                     response = get_response(url)
                     site = BeautifulSoup(response.content, 'html.parser')
                     product_cards = site.find_all('div', class_=re.compile('productCard'))
-                    for dados_do_produto in tags[tag]:
-                        for product_card in product_cards:
-                            titulo_produto = product_card.find('span', class_=re.compile('nameCard')).text
-                            if dados_do_produto['modelo'] in titulo_produto:
-                                price = product_card.find('span', class_=re.compile('priceCard')).text
-                                price_value = [float(i) for i in price.strip('R$').strip().split(',')[:-1]][0]*1000
-                                try:
-                                    if price_value < dados_do_produto['price']:
-                                        print({'ID': dados_do_produto['modelo'], 
-                                            'Title': titulo_produto, 
-                                            'Value': price.replace(u'\xa0', u' ')}) # enviar ao banco de dados
-                                        
-                                        planilha_api.atualizar_planilha('Produtos Kabum!G' + dados_do_produto['linha'], price.replace(u'\xa0', u' '))
-                                        
-                                        if price_value < int(dados_do_produto['referencia']):
-                                            print('No precinho! Enviando notificação para o telegram...')
-                                            telegram_foward.send_message(f'Modelo: ' + dados_do_produto['modelo'] + f'\n\n{titulo_produto} \n\n{price}')
-                                        
-                                except Exception as e:
-                                    print(e)  
-            except Exception as e:
-                print(e)
+                    for product_card in product_cards:
+                        titulo_produto = product_card.find('span', class_=re.compile('nameCard')).text
+                        if codigo_do_produto in titulo_produto:
+                            price = product_card.find('span', class_=re.compile('priceCard')).text
+                            price_value = [float(i) for i in price.strip('R$').strip().split(',')[:-1]][0]*1000 
+                            send_product_data(titulo_produto, codigo_do_produto, price)
+                            planilha_api.atualizar_planilha('Produtos Kabum!G' + str(i+5), price.replace(u'\xa0', u' '))
+                            n = 20000
+                            break
+                if price_value < old_price and price_value < referencia:
+                    print('No precinho! Enviando notificação para o telegram...')
+                    telegram_foward.send_message(f'Modelo: ' + codigo_do_produto + f'\n\n{titulo_produto} \n\n{price}')
+        except Exception as e:
+            print(e)
         time.sleep(600)
-
 
 if __name__ == '__main__':
     main()
-                
